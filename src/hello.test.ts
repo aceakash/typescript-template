aba4cad00eff42c3e67adb1049ceef7f9b6535fa
Akash K <akashkurdekar@gmail.com>
2019-12-28 11:55:01 +0000

    move to jest (#1)

--- src/hello.test.ts
+++ src/hello.test.ts
@@ -1,10 +1,9 @@
 import {sayHello} from './hello'
-import test from 'ava'
 
-test('say hello with a name', t => {
-    t.is(sayHello('Akash'), 'Hello, Akash!')
+test('say hello with a name', () => {
+    expect(sayHello('Akash')).toEqual('Hello, Akash!')
 })
 
-test('say hello world if no name provided', t => {
-    t.is(sayHello(), 'Hello, world!')
+test('say hello world if no name provided', () => {
+    expect(sayHello()).toEqual('Hello, world!')
 })
